07574ff2b664b11083f7fc20f0df7aa2f4466e8c
AlexanderFvS <Alexander.FreiherrvonSeld@haw-hamburg.de>
2025-06-29 15:17:20 +0200

    Create led_io.h

---
+++ led_io.h
@@ -0,0 +1,60 @@
+#ifndef LED_IO_H
+#define LED_IO_H
+
+#include <stdbool.h>
+#include "encoder.h"
+
+/*
+ ****************************************************************************************
+ *  @brief      Bekommt eine Bitmaske und schaltet die LEDs in GPIOF an, bzw. aus.
+ *
+ *  @param  		mask: int mit der Bitmaske
+ *
+ *  @return     void
+ ****************************************************************************************/
+ 
+void setLedD(int);
+
+/*
+ ****************************************************************************************
+ *  @brief      Setzt alle LEDs in GPIOD wieder auf 0.
+ *
+ *
+ *  @return     void
+ ****************************************************************************************/
+
+void resetLedD();
+
+/*
+ ****************************************************************************************
+ *  @brief      Bekommt eine Bitmaske und schaltet die LEDs in GPIOF an, bzw. aus.
+ *
+ *  @param  		offset: int mit dem Offset der entsprechenden LED
+ *
+ *  @return     void
+ ****************************************************************************************/
+
+void setLedE(Direction dir);
+
+/*
+ ****************************************************************************************
+ *  @brief      Liest die Inputs an GPIOF ein.
+ *
+ *
+ *  @return     int: PF0, PF1
+ ****************************************************************************************/
+
+int readLedF();
+
+/*
+ ****************************************************************************************
+ *  @brief      Prüft ob ein Input an GPIOF geschaltet ist.
+ *
+ *  @param  		offset: int mit dem Offsets des Inputs
+ *
+ *  @return     bool: true wenn geschaltet, false wenn nicht.
+ ****************************************************************************************/
+
+bool readButtonF(int);
+
+#endif
